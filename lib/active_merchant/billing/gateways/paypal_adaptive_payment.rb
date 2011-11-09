e5a144a07ee185436f7acda8f36dbf2bca471c9a
levirosol <levi.rosol@gmail.com>
2011-11-09 11:13:43 -0600

    adding the preapproval key to the pay request

--- lib/active_merchant/billing/gateways/paypal_adaptive_payment.rb
+++ lib/active_merchant/billing/gateways/paypal_adaptive_payment.rb
@@ -90,6 +90,9 @@ module ActiveMerchant #:nodoc:
             x.errorLanguage opts[:error_language] ||= 'en_US'
           end
           x.actionType 'PAY'
+          puts "**************"  if opts.key?(:preapproval_key)
+          puts opts[:preapproval_key] if opts.key?(:preapproval_key)
+          puts "**************"  if opts.key?(:preapproval_key)
           x.senderEmail opts[:sender_email] if
             opts.key?(:sender_email)
           x.cancelUrl opts[:cancel_url]
